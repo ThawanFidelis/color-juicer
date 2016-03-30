25eec4da63f0e9f29609b1cba3c42df3212422ac
Thawan Fidelis <thawan.fidelis@gmail.com>
2016-03-30 20:43:37 -0300

    Toggle menu on mobile devices

--- app/assets/javascripts/juicers.js
+++ app/assets/javascripts/juicers.js
@@ -1,2 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+
+$(document).on('click', '.js-menu-toggle', function () {
+  $('.js-menu-target').slideToggle();
+});
