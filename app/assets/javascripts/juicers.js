f4af9daa40e7802e83d675e7f605abe979d9d12b
Thawan Fidelis <thawan.fidelis@gmail.com>
2016-03-30 21:25:37 -0300

    Disable/enable submit button

--- app/assets/javascripts/juicers.js
+++ app/assets/javascripts/juicers.js
@@ -5,3 +5,23 @@
 $(document).on('click', '.js-menu-toggle', function () {
   $('.js-menu-target').slideToggle();
 });
+
+$(function () {
+  $('.js-test-form').bind('submit', function () {
+    disableSubmitButton('.js-test-form');
+  });
+})
+
+function disableSubmitButton (form) {
+  $submitButton = $(form).find('input[type="submit"]');  
+
+  $submitButton.prop('disabled', true);
+  $submitButton.attr('value', 'Gerando...');
+}
+
+function enableSubmitButton (form) {
+  $submitButton = $(form).find('input[type="submit"]');  
+
+  $submitButton.prop('disabled', false);
+  $submitButton.attr('value', 'Gerar novamente');
+}
